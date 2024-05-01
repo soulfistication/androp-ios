a699b2b6ed4c05cf29de0caf032c91b832a8bd90
Ivan Almada <ivan.almada@me.com>
2024-05-01 12:03:04 -0600

    Initial commit

---
+++ androp/andropApp.swift
@@ -0,0 +1,17 @@
+//
+//  andropApp.swift
+//  androp
+//
+//  Created by Iván Almada on 5/1/24.
+//
+
+import SwiftUI
+
+@main
+struct andropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
